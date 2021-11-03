e4d6c54937f2901a283e1f6f4aca6efeab3aa83e
Albert Aleksa <albert.aleksa.by@gmail.com>
2021-11-03 13:09:09 -0500

    Parsing web page using classes
    Add parsing_hw.py
    Modify file website_parsing.py
    
    Classes in Python
    Rewrite webparsing using classes

---
+++ course_files/parsing_hw.py
@@ -0,0 +1,11 @@
+from course_files.website_parsing import WebParser
+
+
+def main():
+    my_parsing = WebParser("https://www.ua-football.com/sport", 'news.txt')
+    my_parsing.run()
+    print(my_parsing.html)
+
+
+if __name__ == '__main__':
+    main()
--- course_files/website_parsing.py
+++ course_files/website_parsing.py
@@ -6,48 +6,52 @@ import ssl
 # https://www.crummy.com/software/BeautifulSoup/bs4/doc.ru/bs4ru.html#
 
 
-def get_html(url):
-    context = ssl._create_unverified_context()
-    req = urllib.request.urlopen(url, context=context)
-    html = req.read()
+class WebParser:
 
-    return html
+    def __init__(self, url, path):
+        self.__url = url
+        self.__path = path
+        self.__raw_html = ''
+        self.__html = ''
+        self.__results = []
+
+    def get_html(self):
+        context = ssl._create_unverified_context()
+        req = urllib.request.urlopen(self.__url, context=context)
+        self.__raw_html = req.read()
+
+    def raw_to_beauty_html(self):
+        self.__html = BeautifulSoup(self.__raw_html, "html.parser")
+
+    def parse_html(self):
+        news = self.__html.find_all('li', class_='liga-news-item')
+
+        for item in news:
+            title = item.find('span', class_='d-block').get_text(strip=True)
+            desc = item.find('span', class_='name-dop').get_text(strip=True)
+            href = item.find('a', class_='liga-news-item-link').get('href')
+            self.__results.append({
+                'title': title,
+                'desc': desc,
+                'href': href
+            })
+
+    def write_to_file(self):
+        with open(self.__path, 'w', encoding='utf-8') as f:
+            i = 1
+            for item in self.__results:
+                f.write(f"News N {i}\n\n Name: {item['title']}\n Description: {item['desc']}\n Link: {item['href']}\n\n"
+                        f"*************************\n")
+                i += 1
+
+    def run(self):
+        self.get_html()
+        self.raw_to_beauty_html()
+        self.parse_html()
+        self.write_to_file()
+
+    @property
+    def html(self):
+        return self.__html.prettify()
 
 
-def parse_html(html_to_parse):
-    soup = BeautifulSoup(html_to_parse, "html.parser")
-    # print(soup.prettify())
-
-    news = soup.find_all('li', class_='liga-news-item')
-    results = []
-
-    for item in news:
-        title = item.find('span', class_='d-block').get_text(strip=True)
-        desc = item.find('span', class_='name-dop').get_text(strip=True)
-        href = item.find('a', class_='liga-news-item-link').get('href')
-        results.append({
-            'title': title,
-            'desc': desc,
-            'href': href
-        })
-
-    return results
-
-
-def write_to_file(lst):
-    with open('news.txt', 'w', encoding='utf-8') as f:
-        i = 1
-        for item in lst:
-            f.write(f"News N {i}\n\n Name: {item['title']}\n Description: {item['desc']}\n Link: {item['href']}\n\n"
-                    f"*************************\n")
-            i += 1
-
-
-def main():
-    html = get_html('https://www.ua-football.com/sport')
-    result_lst = parse_html(html)
-    write_to_file(result_lst)
-
-
-if __name__ == '__main__':
-    main()
