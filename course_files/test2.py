ce5e3cb2ad2a79f3b999908a85fefd8ad5bc59a3
Albert Aleksa <albert.aleksa.by@gmail.com>
2021-11-09 08:54:07 -0600

    Test in Python.
    Add file test2.py

---
+++ course_files/test2.py
@@ -0,0 +1,21 @@
+import math
+
+print(math.e)
+print(math.pow(2, 4))
+
+result = math.e != math.pow(2, 4)
+print(int(result))
+
+print('Mike' > "Mikey")
+
+x = '\''
+print(len(x))
+
+
+
+cash = 19867324678987.99  # DON'T CHANGE THE CASH VARIABLE
+
+# ADD YOUR CODE BELOW:
+print(cash/5)
+res = round(cash/5, 2)
+print(cash, "  ", res)
