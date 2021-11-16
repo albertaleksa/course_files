e71b82c52d8330517594250afdcab87428213ed0
Albert Aleksa <albert.aleksa.by@gmail.com>
2021-11-15 21:00:51 -0600

    Using closure and nonlocal in Python.
    Add file closure.py
    
    Function running_average() returns a function.
    When the function returned is passed a value,
    the function returns the current average of
    all previous function calls

---
+++ p3_bootcamp/closure.py
@@ -0,0 +1,27 @@
+def running_average():
+    """
+    Using closure.
+    Function returns a function.
+    When the function returned is passed a value,
+    the function returns the current average of all previous function calls
+    """
+    avg = 0
+    count = 0
+
+    def helper(num):
+        nonlocal avg
+        nonlocal count
+        count += 1
+        avg = (avg * (count-1) + num) / count
+        return round(avg, 2)
+    return helper
+
+
+rAvg = running_average()
+print(rAvg(10)) # 10.0
+print(rAvg(11)) # 10.5
+print(rAvg(12)) # 11
+
+rAvg2 = running_average()
+print(rAvg2(1)) # 1
+print(rAvg2(3)) # 2
